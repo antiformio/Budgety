669dc54fdaca94dfe33c411b2d3860afc8731606
Filipe Martins <1120170@isep.ipp.pt>
2018-02-13 18:48:22 +0000

    Initialization function.

--- app.js
+++ app.js
@@ -41,35 +41,52 @@ var UIController = (function () {
 
 // Global App Controller
 var controller = (function (budgetCntr, UIcntr) {
+    
+    // Setup eventListeners
+    var setupEventListeners = function () {
 
-    var DOM = UIcntr.getDOMstrings();
+        var DOM = UIcntr.getDOMstrings();
+        
+        // Event handler for btn__add click
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        // Event handler for keypress ("listening" the enter keypress on ALL webpage)
+        document.addEventListener('keypress', function (e) {
+            if (e.keyCode === 13) {
+                ctrlAddItem();
+            }
+
+        });
+
+    };
+    
     
     var ctrlAddItem = function () {
         // TODO : 
         // 1. get the field input Data
-        
+
         var input = UIcntr.getInput();
         console.log(input);
-        
+
         // 2. add item to the budget controller
         // 3. add the item to the UI
         // 4. calculate the budget
         // 5. display the budget on UI
-        
-        
+
+
 
     }
 
-    // Event handler for btn__add click
-    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-    // Event handler for keypress ("listening" the enter keypress on ALL webpage)
-    document.addEventListener('keypress', function (e) {
-
-        if (e.keyCode === 13) {
-            ctrlAddItem();
+    // Initializates the script. 
+    return {
+        init: function() {
+            setupEventListeners();
         }
+    };
 
-    });
 
 })(budgetController, UIController);
+
+
+// Call the return of Global app controller.
+controller.init();
