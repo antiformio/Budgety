3236e8d82b071a81283bf0ba54c37d89c17dc207
Filipe Martins <1120170@isep.ipp.pt>
2018-02-13 11:25:04 +0000

    Added EventListeners (click and keypress) to insert budget button.

--- app.js
+++ app.js
@@ -1,38 +1,43 @@
-// Controller Module
+// Budget Controller
 var budgetController = (function () {
 
-    var x = 23;
 
-    var add = function (a) {
-        return x + a;
-    }
-    // Closures !!
-    return {
-        publicTest: function (b) {
-            return add(b);
-        }
-    }
 
 })();
 
-// UI Module
-var UIController = (function() {
-    
-    
-    
+// UI Controller
+var UIController = (function () {
+
+
+
 })();
 
- 
-// App Module (joins the 2 modules defined before)
-var controller = (function(budgetCntr, UIcntr) {
-    
-    var z = budgetCntr.publicTest(5);
-    
-    // Closures !!
-    return {
-        anotherPublic: function(){
-            console.log(z);
-        }
+
+// Global App Controller
+var controller = (function (budgetCntr, UIcntr) {
+
+    var ctrlAddItem = function () {
+        /*TODO : 
+        1. get the field input Data
+        2. add item to the budget controller
+        3. add the item to the UI
+        4. calculate the budget
+        5. display the budget on UI*/
+
+        console.log('It works');
+
     }
-    
+
+    // Event handler for btn__add click
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    // Event handler for keypress
+    document.addEventListener('keypress', function (e) {
+
+        if (e.keyCode === 13) {
+            ctrlAddItem();
+        }
+
+    });
+
 })(budgetController, UIController);
