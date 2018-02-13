64606feb17ab9049916e550186ee843431dbdf08
Filipe Martins <1120170@isep.ipp.pt>
2018-02-13 13:50:19 +0000

    Reading input data.

--- app.js
+++ app.js
@@ -8,6 +8,32 @@ var budgetController = (function () {
 // UI Controller
 var UIController = (function () {
 
+    // Object for the querySelectors. Easier to refractor if we change the class name on the HTML document
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+
+
+    // Returns an object (composed by functions) to access the private variables of the UIController
+    return {
+        // Returns an object with the input inserted on HTML document, by the user
+        getInput: function () {
+            return {
+                // type is either inc or exp. Check the .add__type class on html document 
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        // Returns the DOMstrings object so other controllers can use it too.
+        getDOMstrings: function () {
+            return DOMstrings;
+        }
+
+    };
 
 
 })();
@@ -16,22 +42,28 @@ var UIController = (function () {
 // Global App Controller
 var controller = (function (budgetCntr, UIcntr) {
 
+    var DOM = UIcntr.getDOMstrings();
+    
     var ctrlAddItem = function () {
-        /*TODO : 
-        1. get the field input Data
-        2. add item to the budget controller
-        3. add the item to the UI
-        4. calculate the budget
-        5. display the budget on UI*/
-
-        console.log('It works');
+        // TODO : 
+        // 1. get the field input Data
+        
+        var input = UIcntr.getInput();
+        console.log(input);
+        
+        // 2. add item to the budget controller
+        // 3. add the item to the UI
+        // 4. calculate the budget
+        // 5. display the budget on UI
+        
+        
 
     }
 
     // Event handler for btn__add click
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
-    // Event handler for keypress
+    // Event handler for keypress ("listening" the enter keypress on ALL webpage)
     document.addEventListener('keypress', function (e) {
 
         if (e.keyCode === 13) {
