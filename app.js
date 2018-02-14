92238e1da883eeea49bb4ab7bcf45c22fec10f8f
Filipe Martins <1120170@isep.ipp.pt>
2018-02-14 17:00:54 +0000

    Adding new Item on the budgetController

--- app.js
+++ app.js
@@ -5,23 +5,23 @@
 
 var budgetController = (function () {
     // FUNCTION CONSTRUCTURE - because we need to instanciate LOTS of expenses and LOTS of incomes objects...
-    
+
     // Custom data type for EXPENSES
     // expenses will have an id, an description and a value
-    var Expense = function(id, description, value) {
-        this.id = id;
-        this.description = description;
-        this.value = value;
-    };
-    
-    // Custom data type for INCOMES
-    var Income = function(id, description, value) {
+    var Expense = function (id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
     };
 
-    
+    // Custom data type for INCOMES
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+
     // Object to store all data: an array of expenses and an array of incomes,
     // and a total expenses and total incomes
     var data = {
@@ -34,9 +34,38 @@ var budgetController = (function () {
             inc: 0
         }
     };
-    
-})();
 
+    // Public method to allow other modules to add a new item to the data structure
+    return {
+        addItem: function (type, desc, val) {
+            var newItem, ID;
+            
+            // ID will be last ID + 1. So check the length of the exp / inc array and subtract 1 , then we have the last id.
+            // now just add 1 to the last ID . When the data structure is empty then the new ID should be zero.
+            if(data.allItems[type].length > 0){
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+            
+
+            // Create the new Item based on 'inc' or 'exp' type
+            if (type === 'exp') {
+                newItem = new Expense(ID, desc, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, desc, val);
+            }
+            // Push to data structure
+            data.allItems[type].push(newItem);
+            // Return the new created element.
+            return newItem;
+        },
+        testing: function() {
+            console.log(data);
+        }
+    };
+
+})();
 
 /*
 --------                        --------
@@ -101,14 +130,16 @@ var controller = (function (budgetCntr, UIcntr) {
 
 
     var ctrlAddItem = function () {
+        var input, newItem;
         // TODO : 
         // 1. get the field input Data
-
-        var input = UIcntr.getInput();
-        console.log(input);
-
+        input = UIcntr.getInput();
+        
         // 2. add item to the budget controller
+        newItem = budgetCntr.addItem(input.type, input.description, input.value);
+        
         // 3. add the item to the UI
+        
         // 4. calculate the budget
         // 5. display the budget on UI
 
