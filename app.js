637ffc0b8d2cfe1f8a8bb1d108bd815f5aea1038
Filipe Martins <1120170@isep.ipp.pt>
2018-02-15 12:05:08 +0000

    Input bug fixes. Preparation for the budget calculation

--- app.js
+++ app.js
@@ -39,15 +39,15 @@ var budgetController = (function () {
     return {
         addItem: function (type, desc, val) {
             var newItem, ID;
-            
+
             // ID will be last ID + 1. So check the length of the exp / inc array and subtract 1 , then we have the last id.
             // now just add 1 to the last ID . When the data structure is empty then the new ID should be zero.
-            if(data.allItems[type].length > 0){
+            if (data.allItems[type].length > 0) {
                 ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
             } else {
                 ID = 0;
             }
-            
+
 
             // Create the new Item based on 'inc' or 'exp' type
             if (type === 'exp') {
@@ -90,61 +90,61 @@ var UIController = (function () {
                 // type is either inc or exp. Check the .add__type class on html document 
                 type: document.querySelector(DOMstrings.inputType).value,
                 description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value
+                value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
             };
         },
         // Adds items to the list of incomes or expenses
-        addListItem: function(obj, type) {
+        addListItem: function (obj, type) {
             var html, newHTML, element;
-            
+
             // Create HTML string with placeholder text (%SOMETHING%)
             // Replace what we want to exchange with "%SOMETHING%" so its easier to find later
-            if(type === 'inc'){
+            if (type === 'inc') {
                 element = DOMstrings.incomeContainer;
-                html = '<div class="item clearfix" id="income-&id&"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
-            } else if(type === 'exp'){
+                html = '<div class="item clearfix" id="income-&id&"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value% €</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+            } else if (type === 'exp') {
                 element = DOMstrings.expensesContainer;
-                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value% €</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             }
-            
-            
+
+
             // Replace the placeHolder text with some actual data (data received with the obj)
             newHTML = html.replace('%id%', obj.id);
             newHTML = newHTML.replace('%description%', obj.description);
             newHTML = newHTML.replace('%value%', obj.value);
-            
-            
+
+
             // Insert the HTML into the DOM - insertAdjacentHTML (ver website para referencias sobre o uso)
             // beforeend para inserir antes do fim do container (de expenses ou de incomes...)
             document.querySelector(element).insertAdjacentHTML('beforeend', newHTML);
-            
+
         },
-        
+
         // Clear the input fields after inserting the item on the specific list
-        clearFields: function() {
+        clearFields: function () {
             var fields, fieldsArr;
             // We need to select the description input field and the value input field, we want to clear them
             // after insertion.
             // querySelectorAll returns a list instead of an array. 
             fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
-            
+
             // We need to use the slice method, but slice only acepts arrays and not lists, but we can
             // acess the Array methods by the prototype, and then use the call and pass the fields list and then
             // it will return an array!
             fieldsArr = Array.prototype.slice.call(fields);
-            
+
             // Iterate the array and clear every item (description and value in this case)
             // Current value - value of the array that its currently being processed (in this case, the first
             //                  will be the input description, and then inputa value)
             // Index - from ero to array.length -1
             // Array - entire array
-            fieldsArr.forEach(function(current, index, array) {
+            fieldsArr.forEach(function (current, index, array) {
                 current.value = "";
             });
-            
+
             // Sets the focus on the first element of the array (volta a por o focus na descrição do item)
             fieldsArr[0].focus();
-            
+
         },
         // Returns the DOMstrings variable so other controllers can use it too.
         getDOMstrings: function () {
@@ -177,34 +177,46 @@ var controller = (function (budgetCntr, UIcntr) {
                 ctrlAddItem();
             }
         });
-        
+
 
     };
 
-    
+    var updateBudget = function () {
+
+        // 1. calculate the budget
+
+        // 2. Return the budget
+
+        // 3. Display the budget on the UI
+    };
+
     var ctrlAddItem = function () {
         var input, newItem;
         // TODO : 
         // 1. get the field input Data
         input = UIcntr.getInput();
-        
-        // 2. add item to the budget controller
-        newItem = budgetCntr.addItem(input.type, input.description, input.value);
-        
-        // 3. add the item to the UI
-        UIcntr.addListItem(newItem, input.type);
-        
-        // 4. clear the fields
-        UIcntr.clearFields();
-        
-        // 5. calculate the budget
-        // 6. display the budget on UI
 
+        // Checks if the input description is not empty 
+        // AND if the input value is not NaN (not a number)
+        // AND if the input value is greater than zero
+        if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
+            // 2. add item to the budget controller
+            newItem = budgetCntr.addItem(input.type, input.description, input.value);
+
+            // 3. add the item to the UI
+            UIcntr.addListItem(newItem, input.type);
+
+            // 4. clear the fields
+            UIcntr.clearFields();
+
+            // 5. Calculate and update Budget
+            updateBudget();
+        }
 
 
     };
-    
-    
+
+
 
     // Initializates the script. 
     return {
